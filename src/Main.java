6cc2911c6cfa571c3caa9aadc58a8c441231baa9
Vlad <vladner80@gmail.com>
2024-11-15 17:38:19 +0200

    fifthTask

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
 public class Main {
-    
     public static void main(String[] args) {
         System.out.println("Hello, GitHub Desktop!");
+        System.out.println("Sum: " + sum(3, 7));
+    }
+
+    public static int sum(int a, int b) {
+        return a + b;
     }
 }
